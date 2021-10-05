30388c31bc509c8c421aba6d120d4b170a4729d6
Alisson <thelinuxlich@gmail.com>
2021-10-05 13:55:50 -0300

    change name to bootstrapAtom, fix async execution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-function enrichStandardFields(atomName, data, addStreamData) {
+async function bootstrapAtom(atomName, data, addStreamData) {
   data.generatedValue =
     typeof data.generatedValue === 'number' ? ++data.generatedValue : 1;
-  addStreamData(`atom:${atomName}:complete`, data);
+  await addStreamData(`atom:${atomName}:complete`, data);
   // treating only WS for now
   if (data.transport && data.transport === 'ws') {
     data.origin = `atom:${atomName}:trigger`;
-    addStreamData('transport:ws:trigger', data);
+    await addStreamData('transport:ws:trigger', data);
   }
   if (data.execution && data.process) {
-    addStreamData('execution:trigger', {
+    await addStreamData('execution:trigger', {
       process: data.process,
       id: data.execution,
       payload: data.payload + ' - ' + data.generatedValue,
@@ -16,4 +16,4 @@ function enrichStandardFields(atomName, data, addStreamData) {
   }
 }
 
-export { enrichStandardFields };
+export { bootstrapAtom };
